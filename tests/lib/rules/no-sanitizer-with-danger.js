7f5f8a887810f383e4aa676d2a0b0a1def6e6fbe
DonghyukJacobJang <donghyuk0117@gmail.com>
2018-06-25 17:35:49 -0400

    Add eslint and prettier

--- tests/lib/rules/no-sanitizer-with-danger.js
+++ tests/lib/rules/no-sanitizer-with-danger.js
@@ -2,23 +2,23 @@
  * @fileoverview Prevent usage of dangerous JSX props without sanitizer
  * @author Donghyuk Jang
  */
-'use strict';
+"use strict";
 
 // ------------------------------------------------------------------------------
 // Constants
 // ------------------------------------------------------------------------------
-const MESSAGE = 'Use xss sanitizer with dangerouslySetInnerHTML';
+const MESSAGE = "Use xss sanitizer with dangerouslySetInnerHTML";
 
 // ------------------------------------------------------------------------------
 // Requirements
 // ------------------------------------------------------------------------------
 
-const rule = require('../../../lib/rules/no-sanitizer-with-danger');
-const RuleTester = require('eslint').RuleTester;
+const RuleTester = require("eslint").RuleTester;
+const rule = require("../../../lib/rules/no-sanitizer-with-danger");
 
 const parserOptions = {
   ecmaVersion: 2018,
-  sourceType: 'module',
+  sourceType: "module",
   ecmaFeatures: {
     jsx: true
   }
@@ -29,37 +29,43 @@ const parserOptions = {
 // ------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester({ parserOptions });
-ruleTester.run('no-sanitizer-with-danger', rule, {
+ruleTester.run("no-sanitizer-with-danger", rule, {
   valid: [
     {
-      code: '<App />;'
+      code: "<App />;"
     },
     {
-      code: '<App dangerouslySetInnerHTML={{ __html: sanitizer("<p>with sanitizer</p>") }} />;'
+      code:
+        "<App dangerouslySetInnerHTML={{ __html: sanitizer('<p>with sanitizer</p>') }} />;"
     },
     {
-      code: '<div dangerouslySetInnerHTML={{ __html: sanitizer("<p>with sanitizer</p>") }} />;'
+      code:
+        "<div dangerouslySetInnerHTML={{ __html: sanitizer('<p>with sanitizer</p>') }} />;"
     }
   ],
   invalid: [
     {
-      code: '<div dangerouslySetInnerHTML={{ __html: "" }}></div>;',
+      code: "<div dangerouslySetInnerHTML={{ __html: '' }}></div>;",
       errors: [{ message: MESSAGE }]
     },
     {
-      code: '<div dangerouslySetInnerHTML={{ __html: "<p>with sanitizer</p>" }} />;',
+      code:
+        "<div dangerouslySetInnerHTML={{ __html: '<p>with sanitizer</p>' }} />;",
       errors: [{ message: MESSAGE }]
     },
     {
-      code: '<div dangerouslySetInnerHTML={{ __html: sanitize("<p>with sanitizer</p>") }} />;',
+      code:
+        "<div dangerouslySetInnerHTML={{ __html: sanitize('<p>with sanitizer</p>') }} />;",
       errors: [{ message: MESSAGE }]
     },
     {
-      code: '<div dangerouslySetInnerHTML={{ __html: xss("<p>with sanitizer</p>") }} />;',
+      code:
+        "<div dangerouslySetInnerHTML={{ __html: xss('<p>with sanitizer</p>') }} />;",
       errors: [{ message: MESSAGE }]
     },
     {
-      code: '<div dangerouslySetInnerHTML={{ __html: Dompurify.sanitizer("<p>with sanitizer</p>") }} />;',
+      code:
+        "<div dangerouslySetInnerHTML={{ __html: Dompurify.sanitizer('<p>with sanitizer</p>') }} />;",
       errors: [{ message: MESSAGE }]
     }
   ]
